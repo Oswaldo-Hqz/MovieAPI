1e9783c06a30a675a09f32d45637954fb60b4802
Oswaldo-Hqz <oehero@hotmail.com>
2023-04-03 23:48:35 -0600

    DTO creation

---
+++ MovieAPI/DTO/MovieDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieAPI.DTO
+{
+    public class MovieDTO
+    {
+        public string MovieName { get; set; } = null!;
+        public int ReleaseYear { get; set; }
+        public string? Synopsis { get; set; }
+        public IFormFile? Poster { get; set; }
+        public List<int> CategoriesIds { get; set; } = new List<int>();
+    }
+}
---
+++ MovieAPI/DTO/RegisterDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieAPI.DTO
+{
+    public class RegisterDTO
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}

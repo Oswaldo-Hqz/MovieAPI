21b2d26bca5d690ca9df1e5f57dbe34bc6ea259d
Oswaldo-Hqz <oehero@hotmail.com>
2023-04-04 22:13:59 -0600

    Controller for user registration and login

---
+++ MovieAPI/Controllers/AuthController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MovieAPI.Data;
+using MovieAPI.DTO;
+using MovieAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MovieAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _Context;
+        private readonly IConfiguration _config;
+
+        public AuthController(ApplicationDbContext context, IConfiguration config)
+        {
+            _Context = context;
+            _config = config;
+        }
+
+        //public static User user = new User();
+
+        [HttpPost("register"), AllowAnonymous]
+        public async Task<ActionResult<User>> Register(RegisterDTO req)
+        {
+            var email = req.Email.ToLower();
+
+            if (await _Context.Users.AnyAsync(u => u.Email == email))
+            {
+                return BadRequest("Email already exists");
+            }
+
+            byte[] PasswordHash, PasswordSalt;
+            CreatePasswordHash(req.Password, out PasswordHash, out PasswordSalt);
+
+            User user = new User();
+
+            user.FirstName = req.FirstName;
+            user.LastName = req.LastName;
+            user.Email = req.Email;
+            user.PasswordHash = PasswordHash; 
+            user.PasswordSalt = PasswordSalt;
+            user.CreatedDate = DateTime.UtcNow;
+
+            var role = await _Context.Roles.FirstOrDefaultAsync(r => r.Name == "user");
+            user.Role = role;
+
+            _Context.Users.Add(user);
+            await _Context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        [HttpPost("login"), AllowAnonymous]
+        public async Task<ActionResult> Login(LoginDTO req)
+        {
+            var email = req.Email.ToLower();
+            var user = await _Context.Users.SingleOrDefaultAsync(u => u.Email == email);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid email or password");
+            }
+
+            if (!VerifyPasswordHash(req.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return Unauthorized("Invalid email or password");
+            }
+
+            string Token = CreateToken(user);
+
+            return Ok(new { Token = Token });
+        }
+
+        //-------
+
+        private string CreateToken(User user)
+        {
+            var key = Encoding.UTF8.GetBytes(_config.GetSection("Jwt:Key").Value);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.FirstName + " " + user.LastName),
+                    new Claim(ClaimTypes.Role, user.Role.Name),
+                    new Claim(ClaimTypes.Email, user.Email)
+                }),
+                Expires = DateTime.UtcNow.AddHours(_config.GetValue<int>("Jwt:ExpirationTime")),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] PasswordHash, out byte[] PasswordSalt) 
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(password));
+            }
+
+            using (var hmac = new HMACSHA512())
+            {
+                PasswordSalt = hmac.Key;
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(password));
+            }
+
+            if (storedHash.Length != 64)
+            {
+                throw new ArgumentException("Invalid length of password hash (64 bytes expected).", nameof(storedHash));
+            }
+
+            if (storedSalt.Length != 128)
+            {
+                throw new ArgumentException("Invalid length of password salt (128 bytes expected).", nameof(storedSalt));
+            }
+
+            using var hmac = new HMACSHA512(storedSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != storedHash[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

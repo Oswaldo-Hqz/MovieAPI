deee14ece32fbbea527e8b2b632a6aefba7b2868
Oswaldo-Hqz <oehero@hotmail.com>
2023-04-07 15:41:02 -0600

    endpoint to filter movies by different parameters

--- MovieAPI/Controllers/MoviesController.cs
+++ MovieAPI/Controllers/MoviesController.cs
@@ -30,6 +30,65 @@ namespace MovieAPI.Controllers
             return await _context.Movies.ToListAsync();
         }
 
+        [HttpGet("FilterMovies"), Authorize(Roles = "admin,user")]
+        public async Task<ActionResult<IEnumerable<FilterMoviesDTO>>> FilterMovies(
+            [FromQuery] string textSearch = "",
+            [FromQuery] int? categoryId = null,
+            [FromQuery] int? releaseYear = null,
+            [FromQuery] int page = 1,
+            [FromQuery] int pageSize = 10,
+            [FromQuery] string sortBy = "CreatedDate",
+            [FromQuery] bool ascending = false)
+        {
+            // Apply text search in name or synipsis
+            IQueryable<Movie> query = _context.Movies;
+            if (!string.IsNullOrWhiteSpace(textSearch))
+            {
+                query = query.Where(m => m.MovieName.Contains(textSearch) || m.Synopsis.Contains(textSearch));
+            }
+
+            // Apply category filter
+            if (categoryId.HasValue)
+            {
+                query = query.Where(m => m.Categories.Any(c => c.Id == categoryId));
+            }
+
+            // Apply release year filter
+            if (releaseYear.HasValue)
+            {
+                query = query.Where(m => m.ReleaseYear == releaseYear);
+            }
+
+            // Apply ordering
+            if (sortBy == "Year")
+                query = ascending ? query.OrderBy(m => m.ReleaseYear) : query.OrderByDescending(m => m.ReleaseYear);
+            else if (sortBy == "Name")
+                query = ascending ? query.OrderBy(m => m.MovieName) : query.OrderByDescending(m => m.MovieName);
+            else if (sortBy == "Rating")
+                query = ascending ? query.OrderBy(m => m.MoviesRatings.Average(r => r.Rating)) : query.OrderByDescending(m => m.MoviesRatings.Average(r => r.Rating));
+            else
+                query = ascending ? query.OrderBy(m => m.CreatedDate) : query.OrderByDescending(m => m.CreatedDate);
+
+            // Apply pagination
+            var totalMovies = await query.CountAsync();
+            var totalPages = (int)Math.Ceiling((double)totalMovies / pageSize);
+            var movies = await query
+                .Include(m => m.Categories)
+                .Include(m => m.MoviesRatings)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .Select(m => m)
+                .ToListAsync();
+
+            var result = new
+            {
+                TotalMovies = totalMovies,
+                TotalPages = totalPages,
+                Movies = movies
+            };
+            return Ok(result);
+        }
+
         // GET: api/Movies/5
         [HttpGet("{id}"), Authorize(Roles = "admin,user")]
         public async Task<ActionResult<Movie>> GetMovie(int id)
@@ -99,9 +158,14 @@ namespace MovieAPI.Controllers
                 using var stream = new MemoryStream();
                 await req.Poster.CopyToAsync(stream);
                 var posterURL = await SaveImageAsync(movie.Id, stream.ToArray());
+
+                movie.Poster = posterURL;
             }
 
-            return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
+            _context.Movies.Update(movie);
+            await _context.SaveChangesAsync();
+
+            return Ok(movie);
         }
 
         [HttpPut("UpdateMovie/{id}"), Authorize(Roles = "admin")]
@@ -156,7 +220,8 @@ namespace MovieAPI.Controllers
 
         private async Task<string> SaveImageAsync(int movieId, byte[] imageBytes)
         {
-            string folderPath = Path.Combine(_hostingEnvironment.WebRootPath, "MoviesPosters");
+            string rootPath = _hostingEnvironment.WebRootPath == null ? _hostingEnvironment.ContentRootPath : _hostingEnvironment.WebRootPath;
+            string folderPath = Path.Combine(rootPath, "MoviesPosters");
 
             if (!Directory.Exists(folderPath))
             {
---
+++ MovieAPI/DTO/FilterMoviesDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieAPI.DTO
+{
+    public class FilterMoviesDTO
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Synopsis { get; set; }
+        public string PosterUrl { get; set; }
+        public List<string> Categories { get; set; } = new List<string>();
+        public double? AverageRating { get; set; }
+    }
+}
